ffd29efb22932e12cd54f814bda6c968b1307859
TikvaYosef <tikvayosef13@gmail.com>
2021-12-19 14:03:42 +0200

    Add project files.

---
+++ Controllers/API/PersonController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Person.Controllers.API
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        [EnableCors("*,*,*,*")]
+        public ActionResult Index()
+        {
+            Person person1 = new Person("tikva", "yosef", 25, "tikva@gmail.com");
+
+            return View(person1);
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        // POST: Person/Create
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+   
+        // POST: Person/Edit/5
+        [HttpPut]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        [HttpDelete]
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+      
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Person.Models
+{
+    public class Person
+    {
+        public string Fname;
+        public string Lname;
+        public int Age;
+        public string Mail;
+
+        public Person(string Fname, string Lname, int Age, string Mail)
+        {
+            this.Fname = Fname;
+            this.Lname = Lname;
+            this.Age = Age;
+            this.Mail = Mail;
+        }
+
+    }
+}
